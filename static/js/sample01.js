1142e3d8b2ce722a2c1bcf797b223e521f1b4761
April Marquez <amarque5@uwo.ca>
2017-03-27 00:05:30 -0400

    fixed student profiles view

--- static/js/sample01.js
+++ static/js/sample01.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
   var i = 0;
   for (i = 0; i < studentsdatabase.students.length; i++) {
     document.getElementById("name" + (i+1)).innerHTML = studentsdatabase.students[i].name;
-    document.getElementById("bio" + (i+1)).innerHTML = studentsdatabase.students[i].biography;
+    document.getElementById("program" + (i+1)).innerHTML = '<b>' + "Program: " +  '</b>' + studentsdatabase.students[i].program;
+    document.getElementById("bio" + (i+1)).innerHTML = '<b>' + "Biography " +  '</b><br/>' + studentsdatabase.students[i].biography;
+    document.getElementById("website" + (i+1)).innerHTML = '<b>' + "Website: " +  '</b>' +studentsdatabase.students[i].website;
   }
 });
