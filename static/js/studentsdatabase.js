bd2320fc0d5ccc56d831a539e52ed29451e8ffae
April Marquez <amarque5@uwo.ca>
2017-04-03 02:18:17 -0400

    Changed text field to keyword instead of code and added more courses

--- static/js/studentsdatabase.js
+++ static/js/studentsdatabase.js
@@ -5,49 +5,49 @@ var studentsdatabase = {
                   "program": "Sculpture",
                   "year": "1st Year",
                   "website": "http://www.sulafay.com",
-                  "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
+                  "biography": "I am a first year Sculpture student at Yale University. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
                 },
                 {
                   "name": "CHASE BOOKER",
                   "program": "Graphic Design",
                   "year": "2nd Year",
                   "website": "http://www.chasebooker.com",
-                  "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
+                  "biography": "I am a 2nd year Graphic Design student at Yale University. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
                 },
                 {
                   "name": "DANA SINGER",
                   "program": "Photography",
                   "year": "2nd Year",
                   "website": "http://www.dannasinger.photoshelter.com",
-                  "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
+                  "biography": "I am a second year Photography student at Yale University. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
                 },
                 {
                   "name": "ESTEFANIA PUERTA",
                   "program": "Painting & Printmaking",
                   "year": "1st Year",
                   "website": "http://estepuerta.com/home.html",
-                  "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
+                  "biography": "I am a 1st year Painting & Printmaking student at Yale University. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
                 },
                 {
                   "name": "LUKE LIBERA MOORE",
                   "program": "Photography",
                   "year": "1st Year",
                   "website": "http://www.lukeliberamoore.com",
-                  "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
+                  "biography": "I am a first year Photography student at Yale University. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
                 },
                 {
                   "name": "WILLIE STEWART",
                   "program": "Sculpture",
                   "year": "1st Year",
                   "website": "http://www.willie-stewart.com/connect",
-                  "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
+                  "biography": "I am a first year Sculpture student at Yale University. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
                 },
                 {
                   "name": "FRANZISKA VIRGILI",
                   "program": "Graphic Design",
                   "year": "2nd Year",
                   "website": "http://www.virgili.de",
-                  "biography": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
+                  "biography": "I am a second year Graphic Design student at Yale University. Maecenas accumsan tempor ligula, nec euismod eros viverra vitae. Proin pretium libero fringilla, interdum enim vel, nim vel."
                 }
             ]
   }
